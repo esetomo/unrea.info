ab23151e1e2c5f1f8893cae9b156ead7d554b055
佐藤 寛和 <esetomo@mint.local>
2010-09-04 00:16:04 +0900

    sinatra

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '61ad08dbfe4311db54e2f6f074b0defc080c487c90b34bb5a6effb768666419475f7c4576f76053212fdd215e75e2b1d50caeeeb0406229bcabbd74a1114b935';
