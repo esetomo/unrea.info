9890be289fa71ea15b0cce48301fd37c8b376fde
Tomokazu Sato <esetomo@gmail.com>
2011-02-01 00:05:39 +0900

    remove secret

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Unrea::Application.config.secret_token = '38362a0e69f74e627ae0430587a8228fffd507f032c8117a66baa444be5b65f3808c2c3c83f0b286352092e4146a4b4f9beaa485258f5833f8e0cf115d7a0e0d'
--- config/initializers/twitter.rb
+++
@@ -1,4 +0,0 @@
-Twitter.configure do |config|
-  config.consumer_key = 'VW0bNP19ZFhA5k1tW3RhA'
-  config.consumer_secret = 'HSrt4RcTESzW0Z2XPVS2N7tWoznmB9aT96jBTjCrsI'
-end
