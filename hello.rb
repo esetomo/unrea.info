9e0c3249e9a76c3a39d1c8e02161aba077fcf44f
Tomokazu Sato <s-tomo@s-tomo.jp>
2011-02-06 17:40:10 +0900

    .

--- hello.rb
+++ hello.rb
@@ -15,6 +15,12 @@ before do
   end
 end
 
+helpers do
+  def base_url
+    @base_url ||= "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
+  end
+end
+
 get '/' do
   if @twitter
     erb %{
