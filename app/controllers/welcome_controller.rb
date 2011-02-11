decf75717d4e9b17c8484e1d800c61bb476debf7
Tomokazu Sato <s-tomo@s-tomo.jp>
2011-02-12 00:28:05 +0900

    .

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 class WelcomeController < ApplicationController
-  def index
+  def index 
     if current_user
       # @tweets = current_user.twitter.friends_timeline
     end
