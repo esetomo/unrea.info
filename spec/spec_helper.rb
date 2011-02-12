4d05168d2846177d1ffd4faa747b5080c135c5cc
Tomokazu Sato <s-tomo@s-tomo.jp>
2011-02-12 14:28:35 +0900

    .

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'rubygems'
 require 'spork'
 
@@ -24,10 +25,6 @@ end
 #   feel free to delete them.
 #
 
-
-
-
-# -*- coding: utf-8 -*-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
