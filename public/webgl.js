13eee95397ac1ce5e8c0a89b7cee85fa01b58fae
Tomokazu Sato <s-tomo@s-tomo.jp>
2011-04-13 23:09:37 +0900

    .

--- public/webgl.js
+++ public/webgl.js
@@ -66,8 +66,12 @@ function initShaders(){
     shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
     gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
 
+    shaderProgram.textureCoordAttribute = gl.getAttribLocation(shaderProgram, "aTextureCoord");
+    gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
+
     shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
     shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+    shaderProgram.samplerUniform = gl.getUniformLocation(shaderProgram, "uSampler");
 }
 
 var mvMatrix = mat4.create();
@@ -78,28 +82,60 @@ function setMatrixUniforms(){
     gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
 }
 
+var ichigoTexture;
+
+function initTexture(){
+    ichigoTexture = gl.createTexture();
+    ichigoTexture.image = new Image();
+    ichigoTexture.image.onload = function(){
+	handleLoadedTexture(ichigoTexture);
+    }
+    
+    ichigoTexture.image.src = "ichigo.png";
+}
+
+function handleLoadedTexture(texture){
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    // gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
 var ichigoVertexPositionBuffer;
+var ichigoVertexTextureCoordBuffer;
 var ichigoVertexIndexBuffer;
 
 function initBuffers(skin_control){
-    var positions = $(skin_control).find('MeshGeometry3D\\.Positions')[0];
-    var points = $(positions).find('Point3D');
-    var indices = $(skin_control).find('MeshGeometry3D\\.TriangleIndices').text().split(/[\,\n]/);
-    indices.shift();
-
     ichigoVertexPositionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, ichigoVertexPositionBuffer);
 
+    var positions = $(skin_control).find('MeshGeometry3D\\.Positions')[0];
+    var points = $(positions).find('Point3D');
     var vertices = points.map(function(){
 	return [this.getAttribute('X'),
 		this.getAttribute('Y'),
 		this.getAttribute('Z')];
     });
-
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     ichigoVertexPositionBuffer.itemSize = 3;
     ichigoVertexPositionBuffer.numItems = points.length;
 
+    ichigoVertexTextureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, ichigoVertexTextureCoordBuffer);
+    var textureCoordinates = $(skin_control).find('MeshGeometry3D\\.TextureCoordinates')[0];
+    var uvs = $(textureCoordinates).find('Point');
+    var textureCoords = uvs.map(function(){
+	return [this.getAttribute("X"),
+		this.getAttribute("Y")];
+    });
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
+    ichigoVertexTextureCoordBuffer.itemSize = 2;
+    ichigoVertexTextureCoordBuffer.numItems = uvs.length;
+
+    var indices = $(skin_control).find('MeshGeometry3D\\.TriangleIndices').text().split(/[\,\n]/);
+    indices.shift();
     ichigoVertexIndexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ichigoVertexIndexBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(indices), gl.STATIC_DRAW);
@@ -123,6 +159,14 @@ function drawScene(){
 
     gl.bindBuffer(gl.ARRAY_BUFFER, ichigoVertexPositionBuffer);
     gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, ichigoVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, ichigoVertexTextureCoordBuffer);
+    gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, ichigoVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, ichigoTexture);
+    gl.uniform1i(shaderProgram.samplerUniform, 0);
+
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ichigoVertexIndexBuffer);
     setMatrixUniforms();  
     // gl.drawArrays(gl.TRIANGLES, 0, ichigoVertexPositionBuffer.numItems);
@@ -155,9 +199,10 @@ $(document).ready(function(){
 	    var canvas = $('#mycanvas')[0];
 	    initGL(canvas);
 	    initShaders();
+	    initTexture();
 	    initBuffers(skin_control);
 	    
-	    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	    gl.clearColor(0.0, 0.0, 0.5, 1.0);
 	    gl.enable(gl.DEPTH_TEST);
 	    
 	    tick();
